6a7e70a7fff8d90628d4a7f9db73e1a13408b9c8
William Moore <moorewj@oavatos.org>
2020-04-05 16:36:05 -0500

    Added support for the ssh protocol

--- scripts/postData.sh
+++ scripts/postData.sh
@@ -22,4 +22,8 @@ echo \"sunset\": \"$today"T"$timeNoZone":00.000$timezone"\", >> data.json
 echo \"streamDaytime\": \"$streamDaytime\" >> data.json
 echo } >> data.json
 echo "Uploading data.json"
-lftp "$PROTOCOL"://"$USER":"$PASSWORD"@"$HOST":"$IMGDIR" -e "set net:max-retries 1; set net:timeout 20; put data.json; bye"
+if [[ $PROTOCOL == "ssh" ]] ; then
+	scp data.json $USER@$HOST:$IMGDIR
+else
+	lftp "$PROTOCOL"://"$USER":"$PASSWORD"@"$HOST":"$IMGDIR" -e "set net:max-retries 1; set net:timeout 20; put data.json; bye"
+fi
