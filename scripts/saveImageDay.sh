24a3f6a7a59e6781075b7725312ba0350da34482
William Moore <William.Moore@umanitoba.ca>
2020-05-17 10:01:20 -0500

    Save daytime image in a different folder

--- scripts/saveImageDay.sh
+++ scripts/saveImageDay.sh
@@ -2,15 +2,53 @@
 source /home/pi/allsky/config.sh
 source /home/pi/allsky/scripts/filename.sh
 
+echo "DayTime"
+
 cd /home/pi/allsky
 
+# Make a directory to store current night images
+# the 12 hours ago option ensures that we're always using today's date even at high latitudes where civil twilight can start after midnight
+CURRENT=$(date +'%Y%m%d-day')
+echo "Date: $CURRENT"
+mkdir -p images/$CURRENT
+mkdir -p images/$CURRENT/thumbnails
+
+# If we are in darkframe mode, we only save to the dark file
+DARK_MODE=$(jq -r '.darkframe' "$CAMERA_SETTINGS")
+
+if [ $DARK_MODE = "1" ] ; then
+	cp $FULL_FILENAME $DARK_FRAME
+	cp $FULL_FILENAME "liveview-$FILENAME.$EXTENSION"
+	exit 0
+fi
+
+# Subtract dark frame if there is one defined in config.sh
+#if [ -e "$DARK_FRAME" ] ; then
+#	convert "$FULL_FILENAME" "$DARK_FRAME" -compose minus_src -composite -type TrueColor "$FILENAME-processed.$EXTENSION"
+#fi
+
+# Create image to use (original or processed) for liveview in GUI
 IMAGE_TO_USE="$FULL_FILENAME"
+#if [ -e "$DARK_FRAME" ] ; then
+#	IMAGE_TO_USE="$FILENAME-processed.$EXTENSION"
+#fi
+
 cp $IMAGE_TO_USE "liveview-$FILENAME.$EXTENSION"
+
+# Save image in images/current directory
+cp $IMAGE_TO_USE "images/$CURRENT/$FILENAME-$(date +'%Y%m%d%H%M%S')-$1.$EXTENSION"
+
+# Create a thumbnail of the image for faster load in web GUI
+if identify $IMAGE_TO_USE >/dev/null 2>&1; then
+	convert "$IMAGE_TO_USE" -resize 100x75 "images/$CURRENT/thumbnails/$FILENAME-$(date +'%Y%m%d%H%M%S')-$1.$EXTENSION";
+fi
+
+echo -e "Saving $FILENAME-$(date +'%Y%m%d%H%M%S')-$1.$EXTENSION\n" >> log.txt
 
 # If upload is true, create a smaller version of the image and upload it
 if [ "$UPLOAD_IMG" = true ] ; then
 	echo -e "Resizing"
-	echo -e "Resizing $FULL_FILENAME \n" >> log.txt
+	echo -e "$(date +'%Y-%m-%d %H:%M:%S') Resizing $FULL_FILENAME" >> log.txt
 
 	# Create a thumbnail for live view
 	# Here's what I use with my ASI224MC
@@ -19,7 +57,7 @@ if [ "$UPLOAD_IMG" = true ] ; then
 	#convert "$IMAGE_TO_USE" -resize 962x720 -gravity Center -crop 680x720+40+0 +repage "$FILENAME-resize.$EXTENSION";
 
 	echo -e "Uploading\n"
-	echo -e "Uploading $FILENAME-resize.$EXTENSION \n" >> log.txt
+	echo -e "$(date +'%Y-%m-%d %H:%M:%S') Uploading $FILENAME-resize.$EXTENSION\n" >> log.txt
 	if [[ $PROTOCOL == "ssh" ]] ; then
 		scp $FILENAME-resize.$EXTENSION $USER@$HOST:$IMGDIR/$FILENAME-resize.tmp
 		ssh $USER@$HOST -C "mv $IMGDIR/$FILENAME-resize.tmp $IMGDIR/$FILENAME-resize.$EXTENSION"
@@ -27,3 +65,4 @@ if [ "$UPLOAD_IMG" = true ] ; then
 		lftp "$PROTOCOL"://"$USER":"$PASSWORD"@"$HOST":"$IMGDIR" -e "set net:max-retries 1; set net:timeout 20; put $FILENAME-resize.$EXTENSION; bye" &
 	fi
 fi
+#sleep 2
